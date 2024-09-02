75b5653793f20ba0a985fe355e0609f47a171249
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-02 13:18:33 +0300

    create global error handler middleware

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import shopifyRoutes from "./shopify.routes.js";
 import woocommerceRoutes from "./woocommerce.routes.js";
+import globalErrorHandler from "../middleware/globalErrorHandler.js";
 
 const router = express.Router();
 
 router.use("/shopify", shopifyRoutes);
 router.use("/woocommerce", woocommerceRoutes);
+router.use(globalErrorHandler);
 
 export default router;
